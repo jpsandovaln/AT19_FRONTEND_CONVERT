dc8f0bb72568f73ffa024d9593c9d8b1c7ef6294
MartinJAA <serverus15>
2023-01-24 00:15:47 -0400

    Add some changes to UI, nav bar with a base page

--- WEB-CONVERTER/src/com/jalasoft/Web-converter/main.py
+++ WEB-CONVERTER/src/com/jalasoft/Web-converter/main.py
@@ -13,21 +13,21 @@ PATH_LOGO = os.path.join(PATH, 'static', 'img', 'logo.png')
 
 @app.route('/')
 def principal():
-    return render_template('index.html', user_image=PATH_LOGO)
+    return render_template('index.html')
 
 
 @app.route('/', methods=['GET'])
 def display_img():
     """Displays the main page and the logo"""
 
-    return render_template("index.html", user_image=PATH_LOGO)
+    return render_template("index.html")
 
 
 @app.route('/video')
 def video():
     misLenguajes = ("PHP", "Python", "Java", "C#",
                     "JavaScript", "Perl", "Ruby", "Rust")
-    return render_template('video.html', lenguajes=misLenguajes, user_image=PATH_LOGO)
+    return render_template('video.html', lenguajes=misLenguajes)
 
 
 @app.route('/contacto')
