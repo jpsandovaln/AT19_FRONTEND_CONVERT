9cf8ff3db14a6b7f2a576faa2ab7472d8899df39
MartinJAA <serverus15>
2023-01-25 22:17:27 -0400

    commit adding images to the page

--- WEB-CONVERTER/src/com/jalasoft/Web-converter/main.py
+++ WEB-CONVERTER/src/com/jalasoft/Web-converter/main.py
@@ -28,10 +28,17 @@ app.config['UPLOAD_FOLDER'] = PATH_UPLOADS
 app.config['SECRET_KEY'] = 'supersecretkey'
 
 
-class UploadFile(FlaskForm):  # c name and add description
-    file = FileField("File", validators=[InputRequired()])
-    param1 = StringField("Param1", validators=[InputRequired()])
-    param2 = StringField("Param2", validators=[InputRequired()])
+class UploadVideo(FlaskForm):  # c name and add description
+    file_video = FileField("File", validators=[InputRequired()])
+    param1_video = StringField("Param1", validators=[InputRequired()])
+    param2_video = StringField("Param2", validators=[InputRequired()])
+    submit = SubmitField("Convert")
+
+
+class UploadImage(FlaskForm):  # c name and add description
+    file_image = FileField("File", validators=[InputRequired()])
+    param1_image = StringField("Param1", validators=[InputRequired()])
+    # param2_image = StringField("Param2", validators=[InputRequired()])
     submit = SubmitField("Convert")
 
 
@@ -43,28 +50,56 @@ def home():
 
 @app.route('/video', methods=['GET', "POST"])
 def video():
-    form = UploadFile()
+    form = UploadVideo()
 
     if form.validate_on_submit():
 
-        file = form.file.data
+        file = form.file_video.data
         file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
                                  secure_filename(file.filename))
         file.save(file_path)
         uploaded_file = open(file_path, 'rb')
-        output_type = form.param1.data
-        fps = form.param2.data
-        url = 'http://127.0.0.1:5000//videotoimage/zip'
+        output_type = form.param1_video.data
+        fps = form.param2_video.data
+        url = 'http://127.0.0.1:5000/videotoimage/zip'
         data = {'output_file': output_type, 'fps': fps}
         files = {'input_file': uploaded_file}
         response = requests.post(url, files=files, data=data)
+        uploaded_file.close()
         print(response)
         if response.status_code == 200:
             download_link = response.text[:-1].strip("\"")
             return render_template('video.html', form=form, download_link=download_link, output_file = output_type)
         else:
-            return "Sorry"
+            return "Sorry video"
     return render_template('video.html', form=form)
+
+
+@app.route('/image', methods=['GET', "POST"])
+def image():
+    form = UploadImage()
+
+    if form.validate_on_submit():
+
+        file = form.file_image.data
+        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                                 secure_filename(file.filename))
+        file.save(file_path)
+        uploaded_file = open(file_path, 'rb')
+        output_type = form.param1_image.data
+        # aux = form.param2.data
+        url = 'http://127.0.0.1:5000/imagetoimage'
+        data = {'output_file': output_type}
+        files = {'input_file': uploaded_file}
+        response = requests.post(url, files=files, data=data)
+        uploaded_file.close()
+
+        if response.status_code == 200:
+            download_link = response.text[:-1].strip("\"")
+            return render_template('image.html', form=form, download_link=download_link)
+        else:
+            return "Sorry"
+    return render_template('image.html', form=form)
 
 
 if __name__ == '__main__':
