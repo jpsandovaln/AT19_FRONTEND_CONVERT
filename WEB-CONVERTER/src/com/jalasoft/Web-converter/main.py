821e1c1f19f1f9b1fa1c3335fa6940c4701b343f
MartinJAA <serverus15>
2023-01-25 20:42:09 -0400

    commit before separating the classes

--- WEB-CONVERTER/src/com/jalasoft/Web-converter/main.py
+++ WEB-CONVERTER/src/com/jalasoft/Web-converter/main.py
@@ -1,13 +1,25 @@
-from ast import literal_eval
+#
+# @main.py Copyright (c) 2023 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
+import os
+import requests
 from flask import Flask
 from flask import render_template
-from flask import request
-import requests
+from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
-from flask_wtf import FlaskForm, Form
-from wtforms.validators import InputRequired, DataRequired
-from wtforms import FileField, SubmitField, StringField
-import os
+from wtforms import FileField
+from wtforms import SubmitField
+from wtforms import StringField
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 PATH = os.path.realpath(os.path.dirname(__file__))
@@ -16,20 +28,20 @@ app.config['UPLOAD_FOLDER'] = PATH_UPLOADS
 app.config['SECRET_KEY'] = 'supersecretkey'
 
 
-class UploadFile(FlaskForm):
+class UploadFile(FlaskForm):  # c name and add description
     file = FileField("File", validators=[InputRequired()])
-    output_type = StringField("Output_type", validators=[InputRequired()])
-    fps = StringField("Fps", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
+    param1 = StringField("Param1", validators=[InputRequired()])
+    param2 = StringField("Param2", validators=[InputRequired()])
+    submit = SubmitField("Convert")
 
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
+@app.route('/', methods=['GET', "POST"])
+@app.route('/home', methods=['GET', "POST"])
 def home():
     return render_template('index.html')
 
 
-@app.route('/video', methods=['GET',"POST"])
+@app.route('/video', methods=['GET', "POST"])
 def video():
     form = UploadFile()
 
@@ -37,21 +49,18 @@ def video():
 
         file = form.file.data
         file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
-                               secure_filename(file.filename))
+                                 secure_filename(file.filename))
         file.save(file_path)
         uploaded_file = open(file_path, 'rb')
-        output_type = form.output_type.data
-        fps = form.fps.data
-
+        output_type = form.param1.data
+        fps = form.param2.data
         url = 'http://127.0.0.1:5000//videotoimage/zip'
-
         data = {'output_file': output_type, 'fps': fps}
         files = {'input_file': uploaded_file}
         response = requests.post(url, files=files, data=data)
         print(response)
         if response.status_code == 200:
             download_link = response.text[:-1].strip("\"")
-            print(download_link)
             return render_template('video.html', form=form, download_link=download_link, output_file = output_type)
         else:
             return "Sorry"
